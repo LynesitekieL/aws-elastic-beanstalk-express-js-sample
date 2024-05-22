4e775c279874eb1d178706ae40018c763d220fcd
LynesitekieL <2023t1281@pwu.edu.ph>
2024-05-22 17:25:47 +0800

    your commit message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello EvelynM World!'));
+app.get('/', (req, res) => res.send('Hello Welcome EvelynM World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
